1b72c0d4c352c6ab80d2afe75daac7f300a2002a
Yehorborovenskyi <yehorborovenskyi@gmail.com>
2023-01-31 19:28:37 +0300

    fix hw-6-task-06.

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,8 @@ const input = document.querySelector("#validation-input");
 input.addEventListener("blur", onInputFocus);
 
 function onInputFocus(event) {
+  input.classList.remove("invalid", "valid");
   if (event.currentTarget.value.length === Number(input.dataset.length)) {
-    input.classList.remove("invalid");
-
     return input.classList.add("valid");
   }
   return input.classList.add("invalid");
